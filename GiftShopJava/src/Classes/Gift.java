646134bf9290d07ed1736c176e502e0ab64e6e4e
Daria Primacheva <keedfik2@mail.ru>
2023-10-16 18:23:04 +0300

    maybe works

--- GiftShopJava/src/Classes/Gift.java
+++ GiftShopJava/src/Classes/Gift.java
@@ -1,29 +1,30 @@
 package Classes;
+
 /**
  * класс, описывающий игрушку
  */
 public class Gift {
     /**
-     *идентификатор игрушки
+     * идентификатор игрушки
      */
     private int id;
     /**
-     *счетчик для идентификаторов
+     * счетчик для идентификаторов
      */
     private static int generalId;
     /**
-     *название игрушки
+     * название игрушки
      */
     private String name;
     /**
-     *кол-во игрушек
+     * кол-во игрушек
      */
     private int value;
     /**
-     *частота выпадения игрушки
+     * частота выпадения игрушки
      */
     private int weight;
-    
+
     public Gift(String name, int value, int weight) {
         this.id = generalId;
         generalId++;
@@ -31,36 +32,46 @@ public class Gift {
         this.value = value;
         this.weight = weight;
     }
-    
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getValue() {
         return value;
     }
+
     public void setValue(int value) {
         this.value = value;
     }
+
     public int getWeight() {
         return weight;
     }
+
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public void lowerValue() {
+        this.value--;
     }
 
     @Override
     public String toString() {
         return "Gift [id=" + id + ", name=" + name + ", value=" + value + ", weight=" + weight + "]";
     }
-
 
 }
